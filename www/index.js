f285d1676519b0b1537b615785daf2e49a34da7b
retro <retro@retros-mbp.home>
2022-06-08 14:41:02 +0100

    concat databases

--- www/index.js
+++ www/index.js
@@ -6,17 +6,21 @@ const sqlPromise = await initSqlJs({
 
 async function fetchDB(path) {
     const dataPromise = fetch(path).then(res => res.arrayBuffer());
-    const [SQL, buf] = await Promise.all([sqlPromise, dataPromise])
+    const [SQL, buf] = await Promise.all([sqlPromise, dataPromise]);
     return new SQL.Database(new Uint8Array(buf));
 }
 
 const srcom = await fetchDB("data/srcom.sqlite");
-const extras = await fetchDB("data/extras.sqlite")
+const extras = await fetchDB("data/extras.sqlite");
 
-const res = srcom.exec("SELECT * FROM Run")
-const res2 = extras.exec("SELECT * FROM Run")
+const res = srcom.exec("SELECT * FROM Run");
+const res2 = extras.exec("SELECT * FROM Run");
 
-console.log(res)
-console.log(res2)
+console.log(res);
+console.log(res2);
 
-//console.log(db)
+let res3 = structuredClone(res2);
+
+res3[0]["values"] = res[0]["values"].concat(res2[0]["values"]);
+
+console.log(res3);
