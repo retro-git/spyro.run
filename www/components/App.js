8b097b60a2c1911b43fce106a7e3469abd35cdce
retro <retro@retros-mbp.home>
2022-06-08 17:49:48 +0100

    react app

--- www/components/App.js
+++ www/components/App.js
@@ -1,9 +1,65 @@
 var React = require('react');
+import initSqlJs from "sql.js";
 
-export function App() {
-    return (
-        <div>
-            <h1>test123123</h1>
-        </div>
-    )
+const sqlPromise = await initSqlJs({
+    locateFile: file => `${file}`
+});
+
+async function fetchDB(path) {
+    const dataPromise = fetch(path).then(res => res.arrayBuffer());
+    const [SQL, buf] = await Promise.all([sqlPromise, dataPromise]);
+    return new SQL.Database(new Uint8Array(buf));
+}
+
+const srcom = await fetchDB("data/srcom.sqlite");
+const extras = await fetchDB("data/extras.sqlite");
+
+const games = extras.exec("SELECT tbl_name from sqlite_master WHERE type = 'table'")[0]["values"];
+
+const res = srcom.exec("SELECT * FROM spyro1");
+const res2 = extras.exec("SELECT * FROM spyro1");
+
+console.log(res);
+console.log(res2);
+
+let res3 = structuredClone(res2);
+
+res3[0]["values"] = res[0]["values"].concat(res2[0]["values"]);
+
+console.log(res3);
+
+let unique_games = [...new Set(res3[0]["values"].map(a => a[res3[0]["columns"].indexOf("game")]))];
+let unique_categories = [...new Set(res3[0]["values"].map(a => a[res3[0]["columns"].indexOf("category")]))];
+
+//console.log(unique_games)
+//console.log(unique_categories);
+
+export class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    state = {
+        game: games[0]
+    };
+
+    handleChange(e) {
+        console.log(e.target.value);
+        this.setState({
+            game: e.target.value
+        })
+    };
+
+    render() {
+        return (
+            <div>
+                <h5>Select game:</h5>
+                <select onChange={this.handleChange.bind(this)}>
+                    {games.map((g) => (
+                        <option value={g}>{g}</option>
+                    ))}
+                </select>
+            </div>
+        )
+    }
 }
--- www/index.js
+++ www/index.js
@@ -1,41 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom/client');
 
-import initSqlJs from "sql.js";
 import { App } from './components/App.js'
-
-const sqlPromise = await initSqlJs({
-    locateFile: file => `${file}`
-});
-
-async function fetchDB(path) {
-    const dataPromise = fetch(path).then(res => res.arrayBuffer());
-    const [SQL, buf] = await Promise.all([sqlPromise, dataPromise]);
-    return new SQL.Database(new Uint8Array(buf));
-}
-
-const srcom = await fetchDB("data/srcom.sqlite");
-const extras = await fetchDB("data/extras.sqlite");
-
-console.log(extras.exec("SELECT tbl_name from sqlite_master WHERE type = 'table'"));
-
-const res = srcom.exec("SELECT * FROM spyro1");
-const res2 = extras.exec("SELECT * FROM spyro1");
-
-console.log(res);
-console.log(res2);
-
-let res3 = structuredClone(res2);
-
-res3[0]["values"] = res[0]["values"].concat(res2[0]["values"]);
-
-console.log(res3);
-
-let unique_games = [...new Set(res3[0]["values"].map(a => a[res3[0]["columns"].indexOf("game")]))];
-let unique_categories = [...new Set(res3[0]["values"].map(a => a[res3[0]["columns"].indexOf("category")]))];
-
-//console.log(unique_games)
-//console.log(unique_categories);
 
 const appElement = document.getElementById('app');
 
