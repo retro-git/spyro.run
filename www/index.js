3905d5a599f35e72f617794cf5ff12907928c6ef
retro <retro@retros-mbp.home>
2022-06-08 15:47:18 +0100

    cur

--- www/index.js
+++ www/index.js
@@ -13,8 +13,10 @@ async function fetchDB(path) {
 const srcom = await fetchDB("data/srcom.sqlite");
 const extras = await fetchDB("data/extras.sqlite");
 
-const res = srcom.exec("SELECT * FROM Run");
-const res2 = extras.exec("SELECT * FROM Run");
+//console.log(extras.exec("SELECT tbl_name from sqlite_master WHERE type = 'table'"));
+
+const res = srcom.exec("SELECT * FROM spyro1");
+const res2 = extras.exec("SELECT * FROM spyro1");
 
 console.log(res);
 console.log(res2);
@@ -24,3 +26,9 @@ let res3 = structuredClone(res2);
 res3[0]["values"] = res[0]["values"].concat(res2[0]["values"]);
 
 console.log(res3);
+
+let unique_games = [...new Set(res3[0]["values"].map(a => a[res3[0]["columns"].indexOf("game")]))];
+let unique_categories = [...new Set(res3[0]["values"].map(a => a[res3[0]["columns"].indexOf("category")]))];
+
+//console.log(unique_games)
+//console.log(unique_categories);
