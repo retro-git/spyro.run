f1d4eb6b37b24a20b49cceb5c46e00847beaceb1
retro <retro@retros-mbp.home>
2022-06-12 12:09:12 +0100

    refactor overrides code

--- src/components/Run.js
+++ src/components/Run.js
@@ -4,32 +4,38 @@ import Base64 from 'crypto-js/enc-base64';
 import overrides from '../data/overrides.json5';
 import styled, { css } from 'styled-components'
 
+const checkColour = (props) => {
+    if (props.cheated) {
+        return 'red';
+    } else return 'black';
+}
+
 const Row = styled.tr`
-  color: black;
+  color: ${checkColour};
 `;
 
 export class Run extends React.Component {
     render() {
-        const picked = _.pick(Object.fromEntries(this.props.r.map((e, i) => [this.props.columns[i], e])), ['game', 'category', 'player', 'time', 'date']);
+        let r = Object.fromEntries(this.props.r.map((e, i) => [this.props.columns[i], e]));
+        const picked = _.pick(r, ['game', 'category', 'player', 'time', 'date']);
         const hash = Base64.stringify(sha256(JSON.stringify(picked)));
-        const override = overrides[hash];
-        return <Row key={this.props.i}>
-            {this.props.r.map((data, index) => {
-                if (override !== undefined && override[this.props.columns[index]] !== undefined) {
-                    data = override[this.props.columns[index]];
-                }
-                switch (index) {
-                    case this.props.columns.indexOf("game"):
+        _.assign(r, overrides[hash]);
+
+        return <Row key={this.props.i} cheated={r["cheated"]}>
+            {Object.keys(r).map((key, index) => {
+                const data = r[key];
+                switch (key) {
+                    case "game":
                         return
-                    case this.props.columns.indexOf("category"):
+                    case "category":
                         return
-                    case this.props.columns.indexOf("subcategory"):
+                    case "subcategory":
                         return
-                    case this.props.columns.indexOf("player"):
+                    case "player":
                         return <td key={index}>({this.props.i + 1}) {data}</td>
-                    case this.props.columns.indexOf("emulated"):
+                    case "emulated":
                         return <td key={index}>{data ? "Yes" : "No"}</td>
-                    case this.props.columns.indexOf("time"):
+                    case "time":
                         return (
                             <td key={index}>
                                 <button key={index} className="button" onClick={() => navigator.clipboard.writeText(hash)}>
