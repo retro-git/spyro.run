b5ad73099c8a1d744d73e13dd3286cf2512431bc
retro <retro@retros-mbp.home>
2022-06-14 15:52:52 +0100

    change url

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ var ReactDOM = require('react-dom/client');
 import { Home } from './components/Home.js'
 
 const appElement = document.getElementById('app');
-ReactDOM.createRoot(appElement).render(<Home/>);
+//ReactDOM.createRoot(appElement).render(<Home/>);
