f7a8d8ad81eaf8bdcbd8e435be54f95e0105b922
retro <retro@retros-mbp.home>
2022-06-18 12:27:54 +0100

    add ods to sqlite script

--- dump-leaderboards.sh
+++ dump-leaderboards.sh
@@ -2,6 +2,12 @@
 
 cd src-dump
 
+rm -rf out
+
 while IFS= read -r line || [ -n "$line" ]; do
     eval "python3 main.py $line --sqlite"
 done < ../leaderboards.txt
+
+cd ..
+
+cp -R src-dump/out/srcom.sqlite src/assets/db
