409ae8ee031ab69c96770b80e935021d474ed054
retro <retro@retros-mbp.home>
2022-06-19 16:06:22 +0100

    make entire label clickable for checkboxes

--- src/components/Legend.js
+++ src/components/Legend.js
@@ -2,19 +2,18 @@ var React = require('react');
 import styled, { css, createGlobalStyle } from 'styled-components'
 
 const LegendSpan = styled.div`
-    margin: 0em 0.2em;
-    padding: 0.2em;
+    margin: 0.1em 0.2em;
+    padding: 0.3em;
     border-radius: 0.5em;
 `;
 
 export class Legend extends React.Component {
     render() {
+        let txt = !this.props.l ? (this.props.name ? this.props.name : _.startCase(this.props.type + " unknown")) : "text" in this.props.l ? this.props.l.text : this.props.name;
         return (
             <LegendSpan style={{ backgroundColor: this.props.l ? this.props.l["colour"] : "grey" }}>
-                {!this.props.l ? (this.props.name ? this.props.name : _.startCase(this.props.type + " unknown")) : "text" in this.props.l ? this.props.l.text : this.props.name}
-                <label>
-                    <input type="checkbox" data-name={this.props.name} data-type={this.props.type} onChange={this.props.handleChangeFilter} checked={this.props.checked} />
-                </label>
+                <label for={txt}>{txt}</label>
+                <input id={txt} type="checkbox" data-name={this.props.name} data-type={this.props.type} onChange={this.props.handleChangeFilter} checked={this.props.checked} />
             </LegendSpan>
         );
     }
